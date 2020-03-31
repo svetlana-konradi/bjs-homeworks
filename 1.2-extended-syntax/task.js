81cb077a7ae91bb1d216ec2fd3056a819177d297
Svetlana Konradi <sv.konradi87@gmail.com>
2020-03-31 11:59:20 +0400

    first task

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,13 @@
+"use strict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let d = Math.pow(b, 2) - 4 * a * c;
+    let x =[];
+    if (d > 0) {
+        x = [((- b + Math.sqrt(d)) / 2 * a), ((- b - Math.sqrt(d)) / 2 * a)];
+    } else if (d == 0) {
+        x = [(-b + 2 * a)];
+    }
+    return x;
 }
 
 function getAverageMark(marks){
