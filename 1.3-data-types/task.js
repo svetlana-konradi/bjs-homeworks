c213d6be23bde56a9c471e26ec5e1561628cf39b
Svetlana Konradi <sv.konradi87@gmail.com>
2020-04-03 20:15:52 +0400

    Second commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,17 +1,18 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let creditAmount = (amount - contribution);
-    let numberMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    let numberMonths = (new Date(date).getFullYear() - new Date().getFullYear()) * 12 + (new Date(date).getMonth() - new Date().getMonth());
     let monthlyPayment = creditAmount * (percent / 100 + ((percent / 100) / (((1 + percent / 100) ** numberMonths) - 1)));
-
     let totalAmount = contribution + monthlyPayment * numberMonths;
 
     return totalAmount;    
 }
 
 function getGreeting(name) {
-    let greeting = (`Привет Мир! Меня зовут ${name}.`)
-    if (name == null || name == undefined || name == ""){
-        greeting = (`Привет Мир! Меня зовут Аноним.`);
+    let greeting = `Привет Мир! Меня зовут ${name}.`
+    if (!name){
+        greeting = `Привет Мир! Меня зовут Аноним.`;
     }
     
     return greeting;
