15658f24708d65b929faf6ddf7e674f972cef7a6
Svetlana Konradi <sv.konradi87@gmail.com>
2020-04-09 14:35:46 +0400

    Initial commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,29 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined){
+        return null;
+    } else {
+        let sound = animal.sound;
+        return sound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let marksSum = 0;
+    for (let i = 0; i < marks.length; i++){
+        marksSum += marks[i];
+    };    
+    let average = marksSum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = +new Date();
+    let diff = now - birthday;
+    let age = diff / (365.25 * 24 * 60 * 60 * 1000);
+    if (age > 18) {
+        return `Вы уже совершеннолетний, что Вы выбрали из карты бара?`;
+    } else {
+        return `Вам еще нет 18, могу предложить Вам молочный коктейль.`
+    }
 }
